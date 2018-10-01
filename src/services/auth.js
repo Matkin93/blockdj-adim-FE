e6bf464c29f460a6963681ad166099077ac9cf86
Robert Coster <rob@signalfire.co.uk>
2018-10-01 11:39:37 +0100

    Added suggested methods to auth service:

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,5 +1,6 @@
 import auth0 from 'auth0-js';
 import config from '../config';
+
 export default class Auth{
     auth0 = new auth0.WebAuth({
         domain: config.domain,
@@ -7,9 +8,35 @@ export default class Auth{
         redirectUri: 'http://localhost:3000/callback',
         responseType: 'token id_token',
         scope: 'openid'
-    })
-
+    }) 
     login = () => {
         this.auth0.authorize();
+    }
+    handleAuthentication = () => {
+        this.auth0.parseHash((err, authResult) => {
+            if (authResult && authResult.accessToken && authResult.idToken){
+                this.setSession(authResult);
+                console.log('I would redirect to home here!!!');
+            }else if(err){
+                console.log(err);
+            }
+        })
+    }
+    setSession = () => {
+        let expiresAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
+        localStorage.setItem('access_token', authResult.accessToken);
+        localStorage.setItem('id_token', authResult.idToken);
+        localStorage.setItem('expires_at', expiresAt);
+        console.log('Set session, redirect to home!!!');
+    }
+    logout = () => {
+        localStorage.removeItem('access_token');
+        localStorage.removeItem('id_token');
+        localStorage.remoteItem('expires_at');
+        console.log('Logout, redirect to home!!!');
+    }
+    isAuthenticated = () => {
+        let expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+        return new Date().getTime() < expiresAt;
     }
 }
