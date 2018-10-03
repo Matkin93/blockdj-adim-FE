b3145b59022b644416eb4603d96c9f3426d8349b
andy hunt <andyhunt200@hotmail.com>
2018-10-03 12:56:46 +0100

    Form for adding new cities added

--- src/components/Areas/index.js
+++ src/components/Areas/index.js
@@ -3,7 +3,8 @@ import AreaSeeds from './seeds';
 
 class Areas extends Component {
     state = {
-        cityId: ''
+        cityId: '',
+        newCity: ''
     }
 
     render() {
@@ -11,11 +12,17 @@ class Areas extends Component {
         const areas = AreaSeeds[1];
         return (
             <div>
+                <form className="add-city" onSubmit={(e) => this.addCity(e)}>
+                    <input onChange={this.typeNewCity} value={this.state.newCity} placeholder="Add a new city"></input>
+                    <button className="submit-city">Add a city</button>
+                </form>
+
                 Area Information by City
             <form>
                     <select onChange={this.selectCity}>
+                        <option key='select' value='Select City'>Select City</option>
                         {cities.map(city => {
-                            return <option value={city.name}>{city.name}</option>
+                            return <option key={city._id} value={city.name}>{city.name}</option>
                         })}
                     </select>
                 </form>
@@ -36,6 +43,22 @@ class Areas extends Component {
         this.setState({
             cityId: city[0]._id
         });
+    }
+
+    typeNewCity = (e) => {
+        this.setState({
+            newCity: e.target.value
+        });
+    }
+
+    addCity = (e) => {
+        e.preventDefault();
+        console.log(this.state.newCity);
+        AreaSeeds[0].push({ 'name': this.state.newCity, _id: "54759eb3c090d83494e2d808" })
+        console.log(AreaSeeds[0]);
+        this.setState({
+            newCity: ''
+        })
     }
 }
 export default Areas;
